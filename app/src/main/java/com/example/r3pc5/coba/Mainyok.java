d3cbc40a7c1bcde638f45df0d3834773b45c7ec0
mauloey <45616439+mauloey@users.noreply.github.com>
2018-12-05 12:31:48 +0700

    tambah TextView

--- app/src/main/java/com/example/r3pc5/coba/Mainyok.java
+++ app/src/main/java/com/example/r3pc5/coba/Mainyok.java
@@ -2,9 +2,13 @@ package com.example.r3pc5.coba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Mainyok extends AppCompatActivity {
 
+    TextView tes1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
